731b8c214ca5af5879460c8b4bf533a9c7ea5cc9
Andrew Park <andysampark@gmail.com>
2022-04-18 00:44:18 -0500

    got some of the search tab done

---
+++ app/src/main/java/com/example/ecrowdpop/Adapter/PostAdapter.java
@@ -0,0 +1,4 @@
+package com.example.ecrowdpop.Adapter;
+
+public class PostAdapter {
+}
--- app/src/main/java/com/example/ecrowdpop/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/ecrowdpop/Fragments/SearchFragment.java
@@ -2,65 +2,220 @@ package com.example.ecrowdpop.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.hendraanggrian.appcompat.socialview.Hashtag;
+import com.hendraanggrian.appcompat.widget.HashtagArrayAdapter;
+import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
+//import com.example.ecrowdpop.Adapter.TagAdapter;
+import com.example.ecrowdpop.Adapter.UserAdapter;
+
+import com.example.ecrowdpop.Model.HashTag;
+import com.example.ecrowdpop.Model.User;
 import com.example.ecrowdpop.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private RecyclerView recyclerViewTags;
+//    private TagAdapter tagAdapter;
+    private UserAdapter userAdapter;
+    private List<User> mUsers;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<HashTag> mTagsPost;
 
-    public SearchFragment() {
-        // Required empty public constructor
-    }
+    private SocialAutoCompleteTextView search_bar;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private List<String> mAvailablehashtags;
+    private List<String> mAvailablehashtagsCount;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view_users);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recyclerViewTags = view.findViewById(R.id.recycler_view_tags);
+        recyclerViewTags.setHasFixedSize(true);
+        recyclerViewTags.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        search_bar = view.findViewById(R.id.search_bar);
+        mTagsPost = new ArrayList<>();
+        mAvailablehashtags = new ArrayList<>();
+        mAvailablehashtagsCount = new ArrayList<>();
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext() , mUsers , true);
+        recyclerView.setAdapter(userAdapter);
+
+//        tagAdapter = new TagAdapter(getContext() , mAvailablehashtags , mAvailablehashtagsCount);
+//        recyclerViewTags.setAdapter(tagAdapter);
+
+        readUsers();
+        readTags();
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                filter(s.toString());
+
+            }
+        });
+
+        return view;
+    }
+
+    private void filter(String text) {
+
+        List<String> mSearchtags = new ArrayList<>();
+        List<String> mSearchTagsCount = new ArrayList<>();
+
+        for (String s : mAvailablehashtags){
+            if (s.toLowerCase().contains(text.toLowerCase())){
+                mSearchtags.add(s);
+                mSearchTagsCount.add(mAvailablehashtagsCount.get(mAvailablehashtags.indexOf(s)));
+            }
+        }
+
+//        tagAdapter.filterList(mSearchtags , mSearchTagsCount);
+
+    }
+
+    private void readTags() {
+
+        FirebaseDatabase.getInstance().getReference().child("HashTags").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mAvailablehashtagsCount.clear();
+                mAvailablehashtags.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    mAvailablehashtags.add(snapshot.getKey());
+                    mAvailablehashtagsCount.add(snapshot.getChildrenCount() + " ");
+                }
+
+//                tagAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void searchUser (String s) {
+
+        recyclerView.setVisibility(View.VISIBLE);
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("username").startAt(s)
+                .endAt(s + "\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    mUsers.add(user);
+                }
+
+                userAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void readUsers () {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (TextUtils.isEmpty(search_bar.getText().toString())){
+                    mUsers.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        User user = snapshot.getValue(User.class);
+                        mUsers.add(user);
+                    }
+
+                    userAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        final ArrayAdapter<Hashtag> hashtagAdapter = new HashtagArrayAdapter<>(getContext());
+
+        FirebaseDatabase.getInstance().getReference().child("HashTags").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mAvailablehashtagsCount.clear();
+                mAvailablehashtags.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    hashtagAdapter.add(new Hashtag(snapshot.getKey() , (int)snapshot.getChildrenCount()));
+                    mAvailablehashtags.add(snapshot.getKey());
+                    mAvailablehashtagsCount.add(snapshot.getChildrenCount() + " ");
+                    Log.d("HashTag" , snapshot.getKey());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        search_bar.setHashtagAdapter(hashtagAdapter);
     }
 }
