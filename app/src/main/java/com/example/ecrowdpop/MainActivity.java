2e58433750f7445946d9bd61cb4f5c65025b91f4
Andrew Park <andysampark@gmail.com>
2022-04-17 23:41:52 -0500

    everything works. the post needs access to gallery/google photos.

--- app/src/main/java/com/example/ecrowdpop/MainActivity.java
+++ app/src/main/java/com/example/ecrowdpop/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.ecrowdpop;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.example.ecrowdpop.Fragments.HomeFragment;
+import com.example.ecrowdpop.Fragments.NotificationFragment;
+import com.example.ecrowdpop.Fragments.ProfileFragment;
+import com.example.ecrowdpop.Fragments.SearchFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,66 +26,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        Bundle intent = getIntent().getExtras();
+        if (intent != null){
+            String publisher = intent.getString("publisherid");
+
+            SharedPreferences.Editor editor = getSharedPreferences("PREFS" , MODE_PRIVATE).edit();
+            editor.putString("profileid" , publisher);
+            editor.apply();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new ProfileFragment()).commit();
+        } else {
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new HomeFragment()).commit();
+
+        }
+
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selecterFragment = new HomeFragment();
+                            break;
+
+                        case R.id.nav_search:
+                            selecterFragment = new SearchFragment();
+                            break;
+
+                        case R.id.nav_add:
+                            selecterFragment = null;
+                            startActivity(new Intent(MainActivity.this , PostActivity.class));
+                            break;
+
+                        case R.id.nav_heart:
+                            selecterFragment = new NotificationFragment();
+                            break;
+
+                        case R.id.nav_profile:
+                            SharedPreferences.Editor editor = getSharedPreferences("PREFS" , MODE_PRIVATE).edit();
+                            editor.putString("profileid" , FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            editor.apply();
+                            selecterFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    if (selecterFragment != null){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , selecterFragment).commit();
+                    }
+
+                    return true;
+                }
+            };
 }
-//        bottomNavigationView = findViewById(R.id.bottom_navigation);
-//
-//        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-//
-//        Bundle intent = getIntent().getExtras();
-//        if (intent != null){
-//            String publisher = intent.getString("publisherid");
-//
-//            SharedPreferences.Editor editor = getSharedPreferences("PREFS" , MODE_PRIVATE).edit();
-//            editor.putString("profileid" , publisher);
-//            editor.apply();
-//
-//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new ProfileFragment()).commit();
-//        } else {
-//
-//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new HomeFragment()).commit();
-//
-//        }
-//
-//    }
-//
-//    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-//            new BottomNavigationView.OnNavigationItemSelectedListener() {
-//                @Override
-//                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//
-//                    switch (menuItem.getItemId()) {
-//                        case R.id.nav_home:
-//                            selecterFragment = new HomeFragment();
-//                            break;
-//
-//                        case R.id.nav_search:
-//                            selecterFragment = new SearchFragment();
-//                            break;
-//
-//                        case R.id.nav_add:
-//                            selecterFragment = null;
-//                            startActivity(new Intent(MainActivity.this , PostActivity.class));
-//                            break;
-//
-//                        case R.id.nav_heart:
-//                            selecterFragment = new NotificationFragment();
-//                            break;
-//
-//                        case R.id.nav_profile:
-//                            SharedPreferences.Editor editor = getSharedPreferences("PREFS" , MODE_PRIVATE).edit();
-//                            editor.putString("profileid" , FirebaseAuth.getInstance().getCurrentUser().getUid());
-//                            editor.apply();
-//                            selecterFragment = new ProfileFragment();
-//                            break;
-//                    }
-//
-//                    if (selecterFragment != null){
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , selecterFragment).commit();
-//                    }
-//
-//                    return true;
-//                }
-//            };
-//}
